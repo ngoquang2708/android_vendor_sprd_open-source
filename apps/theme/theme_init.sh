63864a08dc68c931aff2587f3927d1b9d111021a
zhujianye <zhujy@spreadst.com>
2013-12-05 22:23:30 +0800

    bug#247082 porting uui4 from 4.1_3.4 to 4.4_3.10_app
    [bug number ]247082
    [root cause ]
    [changes ]
    [side effects]
    [reviewers ]
    
    Change-Id: I25f51deaba9f1355a99406241a68d1f891c37c17

---
+++ apps/theme/theme_init.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+# create by spreadst
+
+function create_theme_dir
+{
+  DIR=$1
+  MOD=$2
+  USR=$3
+  GRP=$4
+  mkdir -p $DIR
+  if [ -d $DIR ] ; then
+    chmod $MOD $DIR
+    chown $USR:$GRP $DIR
+  fi
+}
+
+create_theme_dir /data/theme 0775 theme system
+create_theme_dir /data/theme/overlay 0775 theme system
+create_theme_dir /data/theme/additional 0775 theme system
+create_theme_dir /data/theme/default 0775 theme system
+
