10e52386f8161d7eee6a0a8499dec2ddabb6d2d7
yuntao.xiao <yuntao.xiao@spreadtrum.com>
2013-12-08 15:57:34 +0800

    Bug #248152 Add mode for preview directory
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I7f71d91d1949a36d96907c4d982df228d767666b

--- apps/theme/theme_init.sh
+++ apps/theme/theme_init.sh
@@ -17,5 +17,7 @@ function create_theme_dir
 create_theme_dir /data/theme 0775 theme system
 create_theme_dir /data/theme/overlay 0775 theme system
 create_theme_dir /data/theme/additional 0775 theme system
-create_theme_dir /data/theme/default 0775 theme system
-
+create_theme_dir /data/theme/default/ 0775 theme system
+create_theme_dir /data/theme/default/preview 0775 theme system
+create_theme_dir /data/theme/additional/holo 0775 theme system
+create_theme_dir /data/theme/additional/holo/preview 0775 theme system
