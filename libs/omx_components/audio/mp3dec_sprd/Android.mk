b178d5e632c38be5537bcbe7bb44361312883894
wenan.hu <wenan.hu@spreadtrum.com>
2013-12-11 16:16:13 +0800

    Bug #251811 - change aac&mp3 decoder static lib to shared lib
    [bug number  ]251811 - change aac&mp3 decoder static lib to shared lib
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]

--- libs/omx_components/audio/mp3dec_sprd/Android.mk
+++ libs/omx_components/audio/mp3dec_sprd/Android.mk
@@ -17,9 +17,8 @@ LOCAL_CFLAGS := -DOSCL_EXPORT_REF= -DOSCL_IMPORT_REF=
 LOCAL_LDFLAGS += -Wl,--no-warn-shared-textrel
 
 LOCAL_SHARED_LIBRARIES := \
-          libstagefright libstagefright_omx libstagefright_foundation libstagefrighthw libutils libui libbinder libdl libcutils liblog
+          libstagefright libstagefright_omx libstagefright_foundation libstagefrighthw libutils libui libbinder libdl libcutils liblog libomx_mp3dec_sprd
 
-LOCAL_STATIC_LIBRARIES := libmp3dec_sprd
 
 LOCAL_MODULE := libstagefright_sprd_mp3dec
 LOCAL_MODULE_TAGS := optional
