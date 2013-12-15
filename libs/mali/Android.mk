ef54ba71f35a31ad0171abba5e2fa8a0c1babd78
andrew.wu <andrew.wu@spreadtrum.com>
2013-12-15 16:59:46 +0800

    Bug #253684 - Update GPU driver to r4p0
    [bug number  ] 253684
    [root cause  ]
    [changes     ] remove ump from build
    [side effects]
    [reviewers   ]

--- libs/mali/Android.mk
+++ libs/mali/Android.mk
@@ -47,37 +47,6 @@ endif
 
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_PRELINK_MODULE := false
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/src/ump/include \
-	$(LOCAL_PATH)/src/ump/include/ump
-
-LOCAL_SRC_FILES := \
-	src/ump/arch_011_udd/ump_frontend.c \
-	src/ump/arch_011_udd/ump_ref_drv.c \
-	src/ump/arch_011_udd/ump_arch.c \
-	src/ump/os/linux/ump_uku.c \
-	src/ump/os/linux/ump_osu_memory.c \
-	src/ump/os/linux/ump_osu_locks.c
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := libUMP
-
-LOCAL_ARM_MODE := arm
-
-LOCAL_SHARED_LIBRARIES := \
-       libutils \
-       libcutils \
-       libbinder \
-       libdl \
-       libandroid_runtime \
-       liblog
-
-include $(BUILD_SHARED_LIBRARY)
 
 include $(LOCAL_PATH)/driver/Android.mk
 endif
