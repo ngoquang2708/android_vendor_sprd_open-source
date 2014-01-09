7e12232214f8549e9d553719811e6ffc73ffb74e
yifei.zhang <yifei.zhang@spreadtrum.com>
2014-01-09 10:45:26 +0800

    bug#246679 rename the aGPS source & library
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I51160f4561504c020ce6ff40d364566bd38f689c

--- apps/aGPS/generic/Android.mk
+++ apps/aGPS/generic/Android.mk
@@ -20,6 +20,6 @@ LOCAL_SRC_FILES := $(LOCAL_C_COMMON_LIB_SRC)
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE_TAGS := optional
 #-----------------------------------
-LOCAL_SRC_FILES += sprd_l2000.c
-LOCAL_MODULE := libsprd2000agent
+LOCAL_SRC_FILES += sprd_agps_agent.c
+LOCAL_MODULE := libsprd_agps_agent
 include $(BUILD_SHARED_LIBRARY)
