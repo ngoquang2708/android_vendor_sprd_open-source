0c75b5fa7417c2c3ba18de65b8fe8998f41febcb
nan.ji <nan.ji@spreadtrum.com>
2014-02-06 11:32:16 +0800

    Bug #276841 GPL license is detected in some module
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I66e85b3686995b66776da49372a9d64700558a40

--- tools/StabilityTest/vibrator_new.c
+++ tools/StabilityTest/vibrator_new.c
@@ -1,18 +1,3 @@
-/* include/asm/mach-sprd/htc_pwrsink.h
- *
- * Copyright (C) 2008 HTC Corporation.
- * Copyright (C) 2007 Google, Inc.
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/err.h>
