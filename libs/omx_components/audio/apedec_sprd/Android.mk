a5b10a76c540aeffc11b862c302c1c21b2d010ca
leo.wang <pan.wang@spreadtrum.com>
2014-02-21 18:57:43 +0800

    Bug #246303 APE File Support First Version
    [bug number  ] 246303
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: Id87e3b32d203e1201f52e4d84ac67ad32ae40015

---
+++ libs/omx_components/audio/apedec_sprd/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH:= $(call my-dir)
+
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        SPRDAPEDecoder.cpp
+
+LOCAL_C_INCLUDES := \
+        frameworks/av/media/libstagefright/include \
+        frameworks/native/include/media/openmax \
+        frameworks/av/include/media/stagefright \
+        $(TOP)/vendor/sprd/open-source/libs/libstagefrighthw/include \
+        $(TOP)/vendor/sprd/proprietories-source/sprd_codecs/openmax/apedec/decode_src \
+        $(TOP)/vendor/sprd/proprietories-source/sprd_codecs/openmax/apedec/decode_inc \
+
+LOCAL_CFLAGS := -DOSCL_EXPORT_REF= -DOSCL_IMPORT_REF= -D_AACARM_  -D_ARMNINEPLATFORM_  -DAAC_DEC_LITTLE_ENDIAN
+
+LOCAL_SHARED_LIBRARIES := \
+          libstagefright libstagefright_omx libstagefright_foundation libstagefrighthw libutils libui libbinder libdl libcutils
+
+LOCAL_MODULE := libstagefright_sprd_apedec
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+################################################################################
