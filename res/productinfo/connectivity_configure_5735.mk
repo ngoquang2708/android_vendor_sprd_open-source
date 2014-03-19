5a9a9341072670bd454e9f19ee9f6477a7bb7539
wanjinglu <wanjing.lu@spreadtrum.com>
2014-03-19 09:57:57 +0800

    Bug #286304 5735 board type judged by GPIO
    [bug number  ] 286304
    [root cause  ] 5735 has different board type
    [changes     ] add different ini
    [side effects] none
    [reviewers   ] xiangxin.gu
    
    Change-Id: I8a67318e955bf1aa910b749d8159606a66b5240a

--- res/productinfo/connectivity_configure_5735.mk
+++ res/productinfo/connectivity_configure_5735.mk
@@ -2,4 +2,6 @@
 LOCAL_PATH:= vendor/sprd/open-source/res/productinfo
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/scx35_sp5735_connectivity_configure.ini:system/etc/connectivity_configure.ini
+	$(LOCAL_PATH)/scx35_sp5735_connectivity_configure_hw100.ini:system/etc/connectivity_configure_hw100.ini \
+	$(LOCAL_PATH)/scx35_sp5735_connectivity_configure_hw102.ini:system/etc/connectivity_configure_hw102.ini \
+	$(LOCAL_PATH)/scx35_sp5735_connectivity_configure_hw104.ini:system/etc/connectivity_configure_hw104.ini
