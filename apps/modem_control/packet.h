15706f80cd20385903b111f380168eebc9a81959
jiayong.yang <jiayong.yang@spreadtrum.com>
2014-04-14 10:42:08 +0800

    Bug #295467
    [bug number ] 295467
    [root cause ] improve AP boot CP efficiency
    [changes ] modem control
    [side effects] no side effect
    [self test ] test AP boot CP in svlte mode
    [reviewers ] zhiwei.wu
    
    	modified:   apps/modem_control/modem_boot.c
    	modified:   apps/modem_control/packet.c
    
    Change-Id: I62ce09b0968ebad089bf30066ec1951a380e3e2f
    
    	modified:   apps/modem_control/modem_boot.c
    	modified:   apps/modem_control/packet.c
    	modified:   apps/modem_control/packet.h

--- apps/modem_control/packet.h
+++ apps/modem_control/packet.h
@@ -25,9 +25,9 @@
 extern int  send_connect_message(int fd,int flag);
 extern int  send_start_message(int fd,int size,unsigned long addr,int flag);
 extern int  send_end_message(int fd,int flag);
-extern int  send_data_message(int fd,char *buffer,int data_size,int flag);
+extern int  send_data_message(int fd,char *buffer,int data_size,int flag,int image_size,int image_fd);
 extern int  send_exec_message(int fd,unsigned long addr,int flag);
-extern int  uart_send_change_spi_mode_message(int fd);
+extern int  uart_send_change_spi_mode_message(int fd,int max_transfer_size);
 extern unsigned short frm_chk (const unsigned short *src, int len);
 extern unsigned short boot_checksum (const unsigned char *src, int len);
 unsigned short calculate_crc (unsigned short crc, char const *buffer, int len);
