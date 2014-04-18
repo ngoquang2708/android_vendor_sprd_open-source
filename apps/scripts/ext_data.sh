08e9beb36075422e5475fac2719b1a7889ea8913
sally.he <sally.he@spreadtrum.com>
2014-04-19 00:58:05 +0800

    "Bug#266459 Use ip instead of ifconfig"
    [self test] revert 
    This reverts commit cad836a4ddd926c4796b8b1f7cd5c17ffbe4b871

--- apps/scripts/ext_data.sh
+++ apps/scripts/ext_data.sh
@@ -1,20 +1,14 @@
 #!/system/bin/sh
 
 if [ "$1" = "-u" ]; then
-	ifname=`getprop sys.data.setip`
-	ip $ifname
-	ifname=`getprop sys.data.setmtu`
-	ip $ifname
 	ifname=`getprop sys.ifconfig.up`
-	ip $ifname
+	ifconfig $ifname
 	ifname=`getprop sys.data.noarp`
 	ip $ifname
 	setprop sys.ifconfig.up done
 	setprop sys.data.noarp done
 elif [ "$1" = "-d" ]; then
 	ifname=`getprop sys.ifconfig.down`
-	ip $ifname
-	ifname=`getprop sys.data.clearip`
-	ip $ifname
+	ifconfig $ifname
 	setprop sys.ifconfig.down done
 fi
