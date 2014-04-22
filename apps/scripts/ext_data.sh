ae5f899d3ec8ced4244e1d17a4af93552534316c
Yuming Han <yuming.han@spreadtrum.com>
2014-04-22 20:39:27 +0800

    Bug #266459 Support Auto Test
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ] no
    [reviewers   ]
    
    Change-Id: I654aae7e73c36424f9afb14f8e1cd759fef7a514

--- apps/scripts/ext_data.sh
+++ apps/scripts/ext_data.sh
@@ -9,6 +9,23 @@ if [ "$1" = "-u" ]; then
 	ip $ifname
 	ifname=`getprop sys.data.noarp`
 	ip $ifname
+
+#For Auto Test
+	ethup=`getprop sys.gsps.eth.up`
+	if [ "$ethup" = "1" ]; then
+		ifname=`getprop sys.gsps.eth.ifname`
+		localip=`getprop sys.gsps.eth.localip`
+		pcv4addr=`getprop sys.gsps.eth.peerip`
+
+		setprop sys.gsps.eth.up 0
+		ip route add default via $localip dev $ifname
+		iptables -D FORWARD -j natctrl_FORWARD
+		iptables -D natctrl_FORWARD -j DROP
+		iptables -t nat -A PREROUTING -i $ifname -j DNAT --to-destination $pcv4addr
+		iptables -I FORWARD 1 -i $ifname -d $pcv4addr -j ACCEPT
+		iptables -A FORWARD -i rndis0 -o $ifname -j ACCEPT
+		iptables -t nat -A POSTROUTING -s $pcv4addr -j SNAT --to-source $localip
+	fi
 	setprop sys.ifconfig.up done
 	setprop sys.data.noarp done
 elif [ "$1" = "-d" ]; then
@@ -17,4 +34,12 @@ elif [ "$1" = "-d" ]; then
 	ifname=`getprop sys.data.clearip`
 	ip $ifname
 	setprop sys.ifconfig.down done
+
+	ethdown=`getprop sys.gsps.eth.down`
+	if [ "$ethdown" = "1" ]; then
+		setprop sys.gsps.eth.down 0
+		setprop sys.gsps.eth.ifname ""
+		setprop sys.gsps.eth.localip ""
+		setprop sys.gsps.eth.peerip ""
+	fi
 fi
