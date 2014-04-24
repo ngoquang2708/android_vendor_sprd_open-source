bc6df44fb0669cb93ce0c2f942187c283549238f
yangdw <yangdw@spreadst.com>
2014-04-24 11:15:37 +0800

    Bug#304172 can not depart modem assert from modem reset.
    [Bug Number   ]304172
    [Root Cause   ]
    [Changes      ]
    [Side Effects ]
    [Reviewers    ]
    [Feature ID   ]
    [self test    ]pass
    
    Change-Id: Ib2e2984ff4da941ef3570208bb135ba72d47b5e2

--- apps/modemassert/src/com/android/modemassert/AssertApplication.java
+++ apps/modemassert/src/com/android/modemassert/AssertApplication.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.text.TextUtils;
 import android.util.Log;
 import android.app.ActivityManager;
 import android.app.Application;
@@ -30,80 +31,62 @@ import java.lang.reflect.Method;
 
 public class AssertApplication extends Application {
     private final String MTAG = "AssertApplication";
-    private Handler assertHandler;
-    private HandlerThread assertHandlerThread;
+
+    //Action for assert broadcast
     private static final String MODEM_STAT_CHANGE = "com.android.modemassert.MODEM_STAT_CHANGE";
+    //extra data key in intent for assert broadcast
     private static final String MODEM_STAT = "modem_stat";
-    private static final String MODEM_SOCKET_NAME = "modemd";
+    //values of extra data in intent.
     private static final String MODEM_ALIVE = "modem_alive";
     private static final String MODEM_ASSERT = "modem_assert";
-    private static final int MODEM_ASSERT_ID = 1;
-    private static final int BUILD_SOCKET = 2;
-    private static final int BUF_SIZE = 128;
-    private NotificationManager mNm;
-    private CharSequence AssertInfo;
 
-    private final IBinder mBinder = new Binder() {
-        @Override
-        protected boolean onTransact(int code, Parcel data, Parcel reply,
-                int flag) throws RemoteException {
-            return super.onTransact(code, data, reply, flag);
-        }
-    };
+    //name of socket to listen to modem state
+    private static final String MODEM_SOCKET_NAME = "modemd";
+
+    //notification id to cancel
+    private static final int MODEM_ASSERT_ID = 1;
+
+    private static final int BUF_SIZE = 128;
+
+    private final Object mObjectLock = new Object();
+    private Handler mAssertHandler;
+    private HandlerThread mAssertThread;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(MTAG, "onCreate()...");
-        assertHandlerThread = new HandlerThread("assertHandlerThread");
-        assertHandlerThread.start();
-        assertHandler = new assertHandler(assertHandlerThread.getLooper());
-        assertHandler.sendEmptyMessage(BUILD_SOCKET);
+        mAssertThread = new HandlerThread("assertHandlerThread");
+        mAssertThread.start();
+        mAssertHandler = new assertHandler(mAssertThread.getLooper());
+        mAssertHandler.sendEmptyMessage(0);
     }
 
     private class assertHandler extends Handler {
         public assertHandler(Looper looper) {
             super(looper);
         }
+
         @Override
         public void handleMessage(Message msg) {
-            Log.d(MTAG, "handleMessage : "+msg.what);
-            switch (msg.what) {
-                case BUILD_SOCKET:
-                    setupSocket();
-                    break;
-                default:
-                    break;
+            try {
+                LocalSocket socket = new LocalSocket();
+                LocalSocketAddress socketAddr = new LocalSocketAddress(MODEM_SOCKET_NAME,
+                        LocalSocketAddress.Namespace.ABSTRACT);
+                runSocket(socket, socketAddr);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
-    }
-
-    private void setupSocket() {
-        LocalSocket socket = null;
-        LocalSocketAddress socketAddr = null;
-
-        Log.d(MTAG, "setupSocket()...");
-        // create local socket
-        try {
-            socket = new LocalSocket();
-            socketAddr = new LocalSocketAddress(MODEM_SOCKET_NAME,
-                    LocalSocketAddress.Namespace.ABSTRACT);
-        } catch (Exception ex) {
-            Log.w(MTAG, "create client socket Exception" + ex);
-            if (socket == null)
-                Log.w(MTAG, "create client socket error\n");
-        }
-        runSocket(socket, socketAddr);
     }
 
     private void connectToSocket(LocalSocket socket, LocalSocketAddress socketAddr) {
         for (;;) {
             try {
-                Log.d(MTAG, "connectToSocket()...");
                 socket.connect(socketAddr);
                 break;
             } catch (IOException ioe) {
-                Log.w(MTAG, "connect server error\n");
+                ioe.printStackTrace();
                 SystemClock.sleep(10000);
                 continue;
             }
@@ -115,41 +98,43 @@ public class AssertApplication extends Application {
         connectToSocket(socket, socketAddr);
 
         Log.d(MTAG, " -runSocket");
-        synchronized (mBinder) {
+        synchronized (mObjectLock) {
             for (;;) {
                 int cnt = 0;
                 InputStream is = null;
                 try {
                     // mBinder.wait(endtime - System.currentTimeMillis());
                     is = socket.getInputStream();
-                    Log.d(MTAG, "read from socket is : "+is.toString());
                     cnt = is.read(buf, 0, BUF_SIZE);
                     Log.d(MTAG, "read " + cnt + " bytes from socket: \n" );
                 } catch (IOException e) {
                     Log.w(MTAG, "read exception\n");
                 }
                 if (cnt > 0) {
-                    String tempStr = "";
+                    String info = "";
                     try {
-                        tempStr = new String(buf, 0, cnt, "US-ASCII");
+                        info = new String(buf, 0, cnt, "US-ASCII");
                     } catch (UnsupportedEncodingException e) {
-                        // TODO Auto-generated catch block
-                        Log.w(MTAG, "buf transfer char fail\n");
+                        e.printStackTrace();
                     } catch (StringIndexOutOfBoundsException e) {
-                        Log.w(MTAG, "StringIndexOutOfBoundsException\n");
+                        e.printStackTrace();
                     }
-                    AssertInfo = tempStr;
-                    Log.d(MTAG, "read something: "+ tempStr);
-                    if (tempStr.contains("Modem Alive")) {
+                    Log.d(MTAG, "read something: "+ info);
+                    if (TextUtils.isEmpty(info)) {
+                        continue;
+                    }
+                    if (info.contains("Modem Alive")) {
                         sendModemStatBroadcast(MODEM_ALIVE);
                         hideNotification();
-                    } else if (tempStr.length() > 0) {
+                    } else if (info.contains("Modem Assert")) {
                         String value = SystemProperties.get("persist.sys.sprd.modemreset", "default");
-                        Log.d(MTAG, "SystemProperties value is equals 1...after modem assert, modem will be reset.");
+                        Log.d(MTAG, " modemreset ? : " + value);
                         if(!value.equals("1")){
-                            showNotification();
+                            showNotification(info);
                         }
                         sendModemStatBroadcast(MODEM_ASSERT);
+                    } else {
+                        Log.d(MTAG, "do nothing with info :" + info);
                     }
                     continue;
                 } else if (cnt < 0) {
@@ -166,23 +151,21 @@ public class AssertApplication extends Application {
         }
     }
 
-    private void showNotification() {
-        Log.v(MTAG, "showNotefication");
+    private void showNotification(String info) {
+        Log.v(MTAG, "show assert Notefication.");
         int icon = R.drawable.modem_assert;
-        mNm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         long when = System.currentTimeMillis();
-        Notification notification = new Notification(icon, AssertInfo, when);
+        Notification notification = new Notification(icon, info, when);
 
         Context context = getApplicationContext();
         CharSequence contentTitle = "modem assert";
-        CharSequence contentText = AssertInfo;
+        CharSequence contentText = info;
         /** modify 145779 add show assertinfo page **/
         Intent notificationIntent = new Intent(this, AssertInfoActivity.class);
-        notificationIntent.putExtra("assertInfo", AssertInfo);
+        notificationIntent.putExtra("assertInfo", info);
         PendingIntent contentIntent =  PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
-        Log.e(MTAG, "Modem Assert!!!!\n");
-        Log.e(MTAG, "" + contentText.toString());
         //notification.defaults |= Notification.DEFAULT_VIBRATE;
         long[] vibrate = {0, 10000};
         notification.vibrate = vibrate;
@@ -192,17 +175,17 @@ public class AssertApplication extends Application {
         //notification.sound = Uri.parse("file:///sdcard/assert.mp3");
 
         notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-        mNm.notify(MODEM_ASSERT_ID, notification);
+        manager.notify(MODEM_ASSERT_ID, notification);
     }
 
     private void hideNotification() {
         Log.v(MTAG, "hideNotification");
-        mNm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        mNm.cancel(MODEM_ASSERT_ID);
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        manager.cancel(MODEM_ASSERT_ID);
     }
 
     private void sendModemStatBroadcast(String modemStat) {
-        Log.d(MTAG, "sendModemStatBroadcast.....modemStat : "+modemStat);
+        Log.d(MTAG, "sendModemStatBroadcast : " + modemStat);
         Intent intent = new Intent(MODEM_STAT_CHANGE);
         intent.putExtra(MODEM_STAT, modemStat);
         sendBroadcast(intent);
