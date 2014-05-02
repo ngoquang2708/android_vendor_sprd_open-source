ba0a37087239b16f7d2f24c0952eb9065fda8419
Zhaoyang.Yi <zhaoyang.yi@spreadtrum.com>
2014-05-02 14:49:44 +0800

    Bug #307867 solution cam hal2.0 monkey test screen hung
    [bug number  ] Bug307867 solution cam hal2.0 monkey test screen hung
    [root cause  ] solution cam hal2.0 monkey test screen hung
    [changes     ]
    [side effects] no
    [reviewers   ] Baggio He
    [self test   ] board:sp8830ec shark; test: camera cts
    
    Change-Id: I961e674a24678d43fcd1e739badd390a7f03d0c5

--- libs/libcamera/Camera2.mk
+++ libs/libcamera/Camera2.mk
@@ -32,7 +32,6 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_SRC_FILES:= \
 	sc8830/src/SprdOEMCamera.c \
-	sc8830/src/SprdCameraParameters.cpp \
 	sc8830/src/cmr_oem.c \
 	sc8830/src/cmr_set.c \
 	sc8830/src/cmr_mem.c \
