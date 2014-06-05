6dc3569f8ff726153cfa239365ab1c512220c65e
eddy.wei <eddy.wei@spreadtrum.com>
2014-06-05 11:45:39 +0800

    bug#319801 Close camera debug message
    [bug number  ]319801
    [root cause  ]camera debug log is too much
    [changes     ]Close camera debug message
    [side effects]
    [reviewers   ]baggio.he
    [self test   ]sc7730gga camera preview & capture
    
    Change-Id: Ia6c35e89d63f62b533a53c954336f19b2cef3c82

--- libs/libcamera/sc8830/src/sensor_cfg.c
+++ libs/libcamera/sc8830/src/sensor_cfg.c
@@ -63,6 +63,7 @@ extern SENSOR_INFO_T g_HM2058_yuv_info;
 extern SENSOR_INFO_T g_GC2155_yuv_info;
 extern SENSOR_INFO_T g_SP2529_MIPI_yuv_info;
 extern SENSOR_INFO_T g_GC2155_MIPI_yuv_info;
+extern SENSOR_INFO_T g_ov5648_mipi_raw_info;
 
 /**---------------------------------------------------------------------------*
  **                         Constant Variables                                *
@@ -83,8 +84,8 @@ const SENSOR_INFO_T* main_sensor_infor_tab[]=
 	//&g_hi351_mipi_yuv_info,
 	//&g_ov5640_mipi_raw_info,
 	//&g_ov5647_mipi_raw_info,
-	//&g_ov5648_mipi_raw_info,
-      &g_SP2529_MIPI_yuv_info,
+	&g_ov5648_mipi_raw_info,
+	&g_SP2529_MIPI_yuv_info,
 #endif
 #ifdef CONFIG_BACK_CAMERA_CCIR
 	&g_ov5640_yuv_info,
