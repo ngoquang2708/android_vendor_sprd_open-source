5bf05756148495fc6d72e7889a39a9f189257154
Wuling.Zheng <Wuling.Zheng@spreadtrum.com>
2014-07-21 18:27:24 +0800

    Bug #332200 add for hemiao boot animation
    [bug number  ] 332200
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ] yes
    [whether AOB ]
    [reviewers   ]
    
    Change-Id: I2e7f5cd3ebbb9deb2a7836fb070bce285c1f33a3

---
+++ res/boot/boot_res_x3542.mk
@@ -0,0 +1,9 @@
+
+LOCAL_PATH:= vendor/sprd/open-source/res/boot
+
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/bootanimation_x3542.zip:system/media/bootanimation.zip \
+	$(LOCAL_PATH)/bootsound.mp3:system/media/bootsound.mp3 \
+	$(LOCAL_PATH)/shutdownanimation.zip:system/media/shutdownanimation.zip \
+	$(LOCAL_PATH)/shutdownsound.mp3:system/media/shutdownsound.mp3
