254e8527ca7c99519592cb349b90f38f833d2a9d
songbaichen <songbai.chen@spreadtrum.com>
2014-07-24 16:00:22 +0800

    Bug #336632
    [bug number  ] 336632
    [root cause  ] new service
    [changes     ] coverity modify
    [side effects] no
    [self test   ] self test
    [whether AOB ]
    [reviewers   ] mengya.liu@spreadtrum.com ace.an@spreadtrum.com
    
    Change-Id: I0374cf1d26bde4b34a0df8058a40b3696481ec68

--- tools/root_cmd_service/cmd_services.c
+++ tools/root_cmd_service/cmd_services.c
@@ -22,8 +22,8 @@ void *root_cmd_parser(void *arg)
 	int service_fd, connect_fd;
 	socklen_t server_len, client_len;
 	static char recv_buf[MAX_BUF_LEN];
+	static char read_buf[MAX_BUF_LEN];
 	static char send_buf[MAX_BUF_LEN];
-	struct sockaddr_un server_address;
 	struct sockaddr_un client_address;
 
 	unlink(CMD_SERVICE);
@@ -33,6 +33,7 @@ void *root_cmd_parser(void *arg)
 	if ((connect_fd = accept(service_fd, (struct sockaddr *)&client_address, &client_len)) < 0)
 	{
 		ALOGD("accept socket failed!");
+		return NULL;
 	}
 
 	ALOGD("rootcmd_parser service is waitting!\n");
@@ -43,35 +44,44 @@ void *root_cmd_parser(void *arg)
 		if(read(connect_fd, recv_buf, sizeof(recv_buf)) <= 0)
 		{
 			ALOGD("recv cmd error!\n");
+               continue;
 		}
 
 		strcat(recv_buf," 2>&1");
 
-		//ALOGD("recvbuf is %s \n",recv_buf);
 		FILE *file = popen(recv_buf, "r");
 		if(file == NULL)
 		{
-			ALOGD("wrong cmd!\n");
-			write(connect_fd, "wrong cmd", sizeof("wrong cmd"));
-			pclose(file);
+			ALOGD("cannot creat Result file!\n");
 			continue;
+		}
+
+		memset(send_buf, '\0', sizeof(send_buf));
+		if(fread(send_buf, sizeof(char), sizeof(send_buf), file) <= 0)
+		{
+			ALOGD("read command echo error!\n");
+               continue;
 		}
 
 		while(!feof(file))
 		{
-			memset(send_buf, '\0', sizeof(send_buf));
-
-			fread(send_buf, sizeof(char), sizeof(send_buf), file);
-
-			strcat(send_buf,"Result\n");
-			if(write(connect_fd, send_buf, strlen(send_buf)) <= 0)
+			memset(read_buf, '\0', sizeof(read_buf));
+			if(fread(read_buf, sizeof(char), sizeof(read_buf), file) <= 0)
 			{
-				ALOGD("send cmd echo failed!\n");
+				ALOGD("read command echo error!\n");
+				break;
 			}
-
-			//ALOGD("sendbuf is %.*s \n",strlen(send_buf), send_buf);
+			strcat(send_buf,recv_buf);
 		}
+
 		pclose(file);
+		strcat(send_buf,"Result\n");
+		if(write(connect_fd, send_buf, strlen(send_buf)) <= 0)
+		{
+			ALOGD("send cmd echo failed!\n");
+		}
+
+		//ALOGD("sendbuf is %.*s \n",strlen(send_buf), send_buf);
 	}
 
 	return NULL;
@@ -81,6 +91,6 @@ int main(int argc, char *argv[])
 {
 	pthread_t tid_rootcmd_monitor;
 	pthread_create(&tid_rootcmd_monitor,NULL,root_cmd_parser,NULL);
-        pthread_join(tid_rootcmd_monitor , NULL);
+     pthread_join(tid_rootcmd_monitor , NULL);
 	return 0;
 }
