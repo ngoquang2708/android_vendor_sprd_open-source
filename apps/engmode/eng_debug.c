1af304413c67838210e07597e4345716a36759f5
Honghua cao <honghua.cao@spreadtrum.com>
2014-09-10 20:55:15 +0800

    Bug #351077 add engmode debug method that can output englog to a file in data/local/englog
    [bug number  ] 351077
    [root cause  ] engmode log cannt get in calibration mode
    [changes     ] add engmode debug method that can output englog to a file in data/local/englog
    [side effects]
    [self test   ] test by sharkl.the added code is test OK without influence the normal boot up and calibration.
    [whether AOB ]
    [reviewers   ] jiayong.yang
    
    Change-Id: Iea1d98f4cecc1ddfbe8adcc3b651e4ca29067cf4

--- apps/engmode/eng_debug.c
+++ apps/engmode/eng_debug.c
@@ -32,3 +32,61 @@ int eng_file_unlock(int fd)
      return 0;
 }
 
+void* eng_printlog_thread(void *x)
+{
+    int ret;
+
+    ENG_LOG("eng_printlog_thread thread start\n");
+
+    if (0 != access("/data/local/englog",F_OK) ){
+        ret = mkdir("/data/local/englog",S_IRWXU | S_IRWXG | S_IRWXO);
+        if (-1 == ret && (errno != EEXIST)) {
+            ENG_LOG("mkdir /data/local/englog failed.");
+            return 0;
+        }
+    }
+
+    ret = chmod("/data/local/englog",S_IRWXU | S_IRWXG | S_IRWXO);
+    if (-1 == ret) {
+        ENG_LOG("chmod /data/local/englog failed.");
+        exit(0);
+    }
+
+    if (0 == access("/data/local/englog/last_eng.log",F_OK) ){
+        ret = remove("/data/local/englog/last_eng.log");
+        if (-1 == ret) {
+            ENG_LOG("remove failed.");
+            return 0;
+        }
+    }
+
+    if (0 == access("/data/local/englog/eng.log",F_OK) ){
+        ret = rename("/data/local/englog/eng.log","/data/local/englog/last_eng.log");
+        if (-1 == ret) {
+            ENG_LOG("rename failed.");
+            return 0;
+        }
+    }
+
+    ret = creat("/data/local/englog/eng.log",S_IRWXU | S_IRWXG | S_IRWXO);
+    if (-1 == ret && (errno != EEXIST)) {
+        ENG_LOG("creat /data/local/englog/eng.log failed.");
+        return 0;
+    }
+
+    ret = chmod("/data/local/englog/eng.log",0777);
+    if (-1 == ret) {
+        ENG_LOG("chmod /data/local/englog/eng.log failed.");
+        return 0;
+    }
+
+    ret = system("logcat -v threadtime -f /data/local/englog/eng.log -s ENGPC &");
+    if(!WIFEXITED(ret) || WEXITSTATUS(ret) || -1 ==  ret){
+        ENG_LOG(" system failed.");
+        return 0;
+    }
+
+    system("sync");
+
+    return 1;
+}
--- apps/engmode/eng_debug.h
+++ apps/engmode/eng_debug.h
@@ -3,6 +3,7 @@
 
 int eng_file_lock(void);
 int eng_file_unlock(int fd);
+void* eng_printlog_thread(void *x);
 
 #endif /*__ENG_DEBUG_H_*/
 
