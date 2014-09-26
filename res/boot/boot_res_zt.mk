6fc2561c3e13077c190e2871dae136765a733a12
jiangyou.li <jiangyou.li@spreadtrum.com>
2014-09-26 21:19:08 +0800

    Bug #355012 update bootsound.mp3 and shutdownsound.mp3 for SharkL
    [bug number  ] 355012
    [root cause  ] new requirement
    [changes     ] modify the bootsound and shutdownsound
    [side effects] no
    [reviewers   ] huibin.mao
    [self test   ] <board:9620><build ok, boot andh shutdown ok>
    
    Change-Id: I120ff30eeff1a6a7f4819b9e72650e5a6b30dcf5

--- res/boot/boot_res_zt.mk
+++ res/boot/boot_res_zt.mk
@@ -3,6 +3,6 @@ LOCAL_PATH:= vendor/sprd/open-source/res/boot
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/bootanimation_zt.zip:system/media/bootanimation.zip \
-	$(LOCAL_PATH)/bootsound_zt.mp3:system/media/bootsound.mp3 \
+	$(LOCAL_PATH)/bootsound_4g.mp3:system/media/bootsound.mp3 \
 	$(LOCAL_PATH)/shutdownanimation_zt.zip:system/media/shutdownanimation.zip \
-	$(LOCAL_PATH)/shutdownsound_zt.mp3:system/media/shutdownsound.mp3
+	$(LOCAL_PATH)/shutdownsound_4g.mp3:system/media/shutdownsound.mp3
