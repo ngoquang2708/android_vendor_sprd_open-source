13f9b30301a250a380352b99776abe87c560aeb5
baggio.he <baggio.he@spreadtrum.com>
2014-10-03 18:08:22 +0800

    Bug #356739 Add 1080P to picture size
    [bug number  ] 356739
    [root cause  ] video snapshot size is less than 1080P when in video recording
    [changes     ] cmr_oem.c/SprdCameraHardwareConfig.h
    [side effects] none
    [self test   ] CTS
    [whether AOB ] no
    [reviewers   ] jianping.wang
    
    Change-Id: I4da5c03c5a41c1200a565a16fd0d4aff0aac0b0f

--- libs/libcamera/oem/inc/SprdCameraHardwareConfig.h
+++ libs/libcamera/oem/inc/SprdCameraHardwareConfig.h
@@ -510,7 +510,7 @@ struct config_element sprd_back_camera_hardware_config[] = {
 	{"picture-size-values", "2048x1536,1600x1200,1280x960,640x480"},
 	{"video-picture-size-values", "1600x1200,1600x1200,1600x1200,1600x1200,1600x1200"},
 #elif defined(CONFIG_CAMERA_SUPPORT_2M)
-	{"picture-size-values", "1600x1200,1280x960,640x480"},
+	{"picture-size-values", "1920x1088,1600x1200,1280x960,640x480"},
 	{"video-picture-size-values", "1280x960,1280x960,1280x960,1280x960,1280x960"},
 #else
 	{"picture-size-values", "3264x2448,2592x1944,2048x1536,1600x1200,1280x960,640x480"},
