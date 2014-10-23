25fc432ebc0cf0ff0044060b6fd2d0b95e677d32
jojo.ma <jojo.ma@spreadtrum.com>
2014-10-23 15:00:40 +0800

    Bug #351900. Dump blktrace when ANR.
    [bug number  ] 351900
    [root cause  ] dump blktrace when ANR
    [changes     ] dump blktrace when ANR
    [side effects] not found
    [self test   ] feature test
    [whether AOB ]
    [reviewers   ]
    
    Change-Id: Ife464bbe1de536b8eec5d2913e46c005af9ab7c8
    
    Conflicts:
    
    	tools/sprd_res_monitor/sprd_monitor-userdebug.conf

--- tools/sprd_res_monitor/profile/profiledaemon.h
+++ tools/sprd_res_monitor/profile/profiledaemon.h
@@ -1,9 +1,10 @@
-#ifndef __OPROFILE_DAEMON__H__
-#define __OPROFILE_DAEMON__H__
+#ifndef __PROFILE_DAEMON__H__
+#define __PROFILE_DAEMON__H__
 typedef enum
 {
    OPROFILE_START = 0,
    FTRACE_START = 1,
+   BLKTRACE_START = 2,
    OPROFILE_CMD_MAX
 }profilecmd;
 
@@ -20,6 +21,7 @@ extern "C"
     void* profile_daemon(void *);
     int start_oprofile(unsigned long time);
     int start_ftrace(unsigned long time);
+    int start_blktrace();
 #ifdef __cplusplus
 }
 #endif
@@ -27,7 +29,13 @@ extern "C"
 
 #define PROFILE_SOCKET_PATH           "/data/local/tmp/profile/"
 #define PROFILE_SOCKET_NAME           PROFILE_SOCKET_PATH "socket"
-#define OPROFILE_DEBUG_SWITCHER        "debug.oprofile.value"
-#define FTRACE_DEBUG_SWITCHER          "debug.ftrace.value"
+#define OPROFILE_DEBUG_SWITCHER       "debug.oprofile.value"
+#define FTRACE_DEBUG_SWITCHER         "debug.ftrace.value"
+#define BLKTRACE_DEBUG_SWITCHER       "debug.blktrace.value"
+#define BLKTRACE_CONF_FILE            "/system/etc/blktrace.conf"
+#define BLKTRACE_LOG_PATH             "/data/blktrace"
+
+char blk_opt[128];
+char blkcapture[256];
 
 #endif
