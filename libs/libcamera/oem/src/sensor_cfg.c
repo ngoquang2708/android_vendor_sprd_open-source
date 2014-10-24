34fd03f5b54838c995eb7386446c6a72cd1603c5
wenbin.guo <wenbin.guo@spreadtrum.com>
2014-10-24 10:08:21 +0800

    Bug #364415, sharkl rear camera ov5670 bringup
    [bug number  ] 364415
    [root cause  ] ov5670 bringup
    [changes     ] add sensor driver
    [side effects] n/a
    [self test   ] preview and capture
    [whether AOB ] no
    [reviewers   ] tim.zhu
    
    Change-Id: I56765d8599af23abc21d14f69d448e5adb74ffd9

--- libs/libcamera/oem/src/sensor_cfg.c
+++ libs/libcamera/oem/src/sensor_cfg.c
@@ -68,6 +68,7 @@ extern SENSOR_INFO_T g_GC2155_yuv_info;
 extern SENSOR_INFO_T g_SP2529_MIPI_yuv_info;
 extern SENSOR_INFO_T g_GC2155_MIPI_yuv_info;
 extern SENSOR_INFO_T g_ov5648_mipi_raw_info;
+extern SENSOR_INFO_T g_ov5670_mipi_raw_info;
 extern SENSOR_INFO_T g_autotest_ov8825_mipi_raw_info;
 extern SENSOR_INFO_T g_autotest_ov5640_mipi_yuv_info;
 extern SENSOR_INFO_T g_at_ov5640_ccir_yuv_info;
@@ -89,6 +90,7 @@ const SENSOR_INFO_T* main_sensor_infor_tab[]=
 	&g_ov8825_mipi_raw_info,
 	&g_s5k4ec_mipi_yuv_info,
 	&g_ov5648_mipi_raw_info,
+	&g_ov5670_mipi_raw_info,
 	&g_ov8865_mipi_raw_info,
 	&g_ov5640_mipi_yuv_info,
 	&g_hi544_mipi_raw_info,
