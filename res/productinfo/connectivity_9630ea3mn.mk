150a8f8a2a2b61eef3c5d0935b918498c301f5f4
jiawang.yu <jiawang.yu@spreadtrum.com>
2014-11-27 09:41:52 +0800

    Bug #372713 [SharkL]9830 board was changed to the 9630, and 1500MHZ as default
    [bug number  ] 372713
    [root cause  ] Keep the name of unity
    [changes     ] about board
    [side effects] NONE
    [self test   ] Create new board
    [whether AOB ] NONE
    [reviewers   ] Jinyuan.Guo
    
    Change-Id: I5abe24a319104611d6de471dc12176ca02f3e0db

--- res/productinfo/connectivity_9630ea3mn.mk
+++ res/productinfo/connectivity_9630ea3mn.mk
@@ -2,6 +2,6 @@
 LOCAL_PATH:= vendor/sprd/open-source/res/productinfo
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/scx35l_sp9830ea3mn_connectivity_configure.ini:system/etc/connectivity_configure.ini \
-	$(LOCAL_PATH)/scx35l_sp9830ea3mn_connectivity_calibration.ini:system/etc/onnectivity_calibration.ini \
-	$(LOCAL_PATH)/scx35l_sp9830ea3mn_connectivity_calibration.ini:prodnv/connectivity_calibration.ini
+	$(LOCAL_PATH)/scx35l_sp9630ea3mn_connectivity_configure.ini:system/etc/connectivity_configure.ini \
+	$(LOCAL_PATH)/scx35l_sp9630ea3mn_connectivity_calibration.ini:system/etc/onnectivity_calibration.ini \
+	$(LOCAL_PATH)/scx35l_sp9630ea3mn_connectivity_calibration.ini:prodnv/connectivity_calibration.ini
