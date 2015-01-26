56cc266f3641e984fe2f1e7c6cc492a88ccee0e2
honghua.cao <honghua.cao@spreadtrum.com>
2015-01-26 18:32:40 +0800

    Bug #386490 回退sharkl bt/wifi地址前6位固定问题
    [bug number  ] 386490
    [root cause  ] wiriteimei 不能正常写入BT/WIFI 地址
    [changes     ] 回退BT/WIFI 的前6位设置为展讯固定标记
    [side effects] none
    [self test   ] 9630mn/MobileTester读取BT/WIFI 地址正常
    [whether AOB ]
    [reviewers   ] alvin.zhou
    
    Change-Id: Id00fafcf330b00ece885d082137e2ccc489200d6

--- apps/engmode/eng_diag.c
+++ apps/engmode/eng_diag.c
@@ -2208,7 +2208,8 @@ static int eng_diag_btwifiimei(char *buf,int len, char *rsp, int rsplen)
             if((cmd_mask & DIAG_CMD_BTBIT) || (cmd_mask & DIAG_CMD_WIFIBIT)){
                 //write bt address
                 if((head_ptr->subtype&DIAG_CMD_BTBIT)>0) {
-                    sprintf(btaddr, "%s:%02x:%02x:%02x",SPRD_MAC,\
+                    sprintf(btaddr, "%02x:%02x:%02x:%02x:%02x:%02x",\
+                            direct->btaddr[5],direct->btaddr[4],direct->btaddr[3], \
                             direct->btaddr[2],direct->btaddr[1],direct->btaddr[0]);
                     pBtAddr = btaddr;
                     ENG_LOG("%s: BTADDR:%s\n",__func__, btaddr);
@@ -2216,7 +2217,8 @@ static int eng_diag_btwifiimei(char *buf,int len, char *rsp, int rsplen)
 
                 //write wifi address
                 if((head_ptr->subtype&DIAG_CMD_WIFIBIT)>0) {
-                    sprintf(wifiaddr, "%s:%02x:%02x:%02x",SPRD_MAC,\
+                    sprintf(wifiaddr, "%02x:%02x:%02x:%02x:%02x:%02x",\
+                            direct->wifiaddr[0],direct->wifiaddr[1],direct->wifiaddr[2], \
                             direct->wifiaddr[3],direct->wifiaddr[4],direct->wifiaddr[5]);
                     pWifiAddr = wifiaddr;
                     ENG_LOG("%s: WIFIADDR:%s\n",__func__,wifiaddr);
